30701027ce457b3dde2c8ee47a9e297e6be77f47
Vocoshop <vocoshop5@gmail.com>
2026-07-15 16:58:34 +0200

    Boissons : casier, carton en unité de base

--- vocoshop/screens/CreateProductScreen.tsx
+++ vocoshop/screens/CreateProductScreen.tsx
@@ -18,7 +18,7 @@ const CATEGORIES = [
 ];
 
 const UNIT_PRESETS: Record<string, string[]> = {
-  Boissons: ["bouteille", "canette", "litre", "verre", "gobelet"],
+  Boissons: ["bouteille", "canette", "litre", "verre", "gobelet", "casier", "carton"],
   Épicerie: ["kilogramme", "sachet", "paquet", "pièce", "litre"],
   Laitière: ["bouteille", "pot", "pièce", "litre"],
   Boucherie: ["kilogramme", "demi-kilo", "pièce"],
